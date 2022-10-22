6cb3aa91a5da39aad81494616bde4ac2b873d125
jeffoty <odhiambo47@gmail.com>
2022-10-22 19:41:52 +0300

    done

--- contacts.rb
+++ contacts.rb
@@ -1,4 +1,4 @@
-require 'pry'
+require "pry"
 
 # this method returns a hash, which you can pass to the remove_strawberry method:
 # remove_strawberry(contacts)
@@ -7,19 +7,29 @@ def contacts
     "Jon Snow" => {
       name: "Jon",
       email: "jon_snow@thewall.we",
-      favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+      favorite_ice_cream_flavors: ["chocolate", "vanilla"],
     },
     "Freddie Mercury" => {
       name: "Freddie",
       email: "freddie@mercury.com",
-      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-    }
+      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"],
+    },
   }
 end
 
 def remove_strawberry(contacts)
   # your code here!
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
 
 # print the output to the terminal for inspection
 pp remove_strawberry(contacts)
+
